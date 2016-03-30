ea912ae509dd8f66cbab6cd2a966dddfaa11adbb
Sanjeev <chopra@ge.com>
2016-03-30 12:26:23 -0700

    Added enforcement of Predix-Client-Assertion header for 2way TLS.
    Signed-off-by: Irina <irina.epshteyn@ge.com>

--- server/src/test/java/org/cloudfoundry/identity/uaa/provider/token/ClientAssertionHeaderAuthenticatorTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/provider/token/ClientAssertionHeaderAuthenticatorTest.java
@@ -1,97 +1,34 @@
 package org.cloudfoundry.identity.uaa.provider.token;
 
-import java.util.Map;
-
-import org.cloudfoundry.identity.uaa.oauth.token.ClaimConstants;
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.authentication.BadCredentialsException;
 
 public class ClientAssertionHeaderAuthenticatorTest {
     private final static String TENANT_ID = "t10";
-    private final static String DEVICE_ID = "d10";
-    private Long currentTimeSeconds = System.currentTimeMillis() / 1000L;
-    
+    private final static String ISSUER_ID = "d10";
+
     @Test
     public void testSuccess() {
-        String header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds,
-                DEVICE_ID, TENANT_ID);
-        ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-        Map<String, Object> claims = headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
-        Assert.assertEquals(DEVICE_ID, claims.get(ClaimConstants.SUB));
-        Assert.assertEquals(TENANT_ID, claims.get(ClaimConstants.TENANT_ID));
+        try {
+            String header = new MockClientAssertionHeader().mockSignedHeader(ISSUER_ID, TENANT_ID);
+            ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
+            headerAuthenticator.authenticate(header, TestKeys.TOKEN_VERIFYING_KEY);
+        } catch (Exception e) {
+            Assert.fail("Failed to authenticate client assertion header. " + e.getMessage());
+        }
     }
-    
-    @Test(expected=AuthenticationException.class)
-    public void testIncorrectSigningKey() {
-        String header = new MockClientAssertionHeader().mockIncorrectlySignedHeader(DEVICE_ID, TENANT_ID);
-        ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-        headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
-    }    
 
-    @Test(expected=AuthenticationException.class)
-    public void testHeaderTTLExpired() throws InterruptedException {
-        String header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds,
-                DEVICE_ID, TENANT_ID);
-
-        //set TTL for header to 1 second
-        ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator(1);
-        //wait for header to expire
-        Thread.sleep(2000);
-
-        headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
-    }
-    
     @Test
-    public void testIatClaimTimeSkew() throws InterruptedException {
+    public void testFail() {
         try {
-            String header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds - 6, DEVICE_ID, TENANT_ID);
+            String header = new MockClientAssertionHeader().mockIncorrectlySignedHeader(ISSUER_ID, TENANT_ID);
             ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-            headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
-            Assert.fail("authenticate did not fail when iat is skewed by more than 5 seconds");
-        } catch (AuthenticationException e) {
-            //expected
+            headerAuthenticator.authenticate(header, TestKeys.TOKEN_VERIFYING_KEY);
+            Assert.fail("Succeeded to authenticate incorrectly signed client assertion header.");
+        } catch (BadCredentialsException bce) {
+        } catch (Exception e) {
+            Assert.fail("Validation of client assertion header failed for unexpected reason.");
         }
-        
-        currentTimeSeconds = System.currentTimeMillis() / 1000L;
-        try {
-            String header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds + 6, DEVICE_ID, TENANT_ID);
-            ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-            headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
-            Assert.fail("authenticate did not fail when iat is skewed by more than 5 seconds");
-        } catch (AuthenticationException e) {
-            //expected
-        }
-
-        currentTimeSeconds = System.currentTimeMillis() / 1000L;
-        String header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds + 3, DEVICE_ID, TENANT_ID);
-        ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-        Assert.assertNotNull(headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY));
-        
-        currentTimeSeconds = System.currentTimeMillis() / 1000L;
-        header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds - 3, DEVICE_ID, TENANT_ID);
-        headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-        Assert.assertNotNull(headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY));
-    }
-
-    @Test(expected=AuthenticationException.class)
-    public void testIatClaimAbsent() {
-        String header = new MockClientAssertionHeader().mockSignedHeader(null, DEVICE_ID, TENANT_ID);
-        ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-        headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
-    }
-
-    @Test(expected=AuthenticationException.class)
-    public void testTenantIdClaimAbsent() {
-        String header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds, DEVICE_ID, null);
-        ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-        headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
-    }
-
-    @Test(expected=AuthenticationException.class)
-    public void testSubClaimAbsent() {
-        String header = new MockClientAssertionHeader().mockSignedHeader(currentTimeSeconds, null, TENANT_ID);
-        ClientAssertionHeaderAuthenticator headerAuthenticator = new ClientAssertionHeaderAuthenticator();
-        headerAuthenticator.authenticate(header, MockKeyProvider.DEVICE1_PUBLIC_KEY);
     }
 }
