f057ca76c767a7f096d69ded67a9f2518fb3f9de
Jeremy Morony <jmorony@pivotal.io>
2019-10-02 16:29:04 -0700

    Enabled the session store to be configurable.
    The choices are: memory or database
    
    [#168911564]
    
    Signed-off-by: Stephane Jolicoeur <sjolicoeur@pivotal.io>
    Co-authored-by: Stephane Jolicoeur <sjolicoeur@pivotal.io>

--- server/src/main/java/org/cloudfoundry/identity/uaa/web/beans/UaaSessionConfig.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/beans/UaaSessionConfig.java
@@ -1,13 +1,63 @@
 package org.cloudfoundry.identity.uaa.web.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.session.jdbc.config.annotation.web.http.JdbcHttpSessionConfiguration;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.web.context.annotation.RequestScope;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class UaaSessionConfig {
+    private static final String DATABASE_SESSION_STORE_TYPE = "database";
+
+    @Configuration
+    @Conditional(DatabaseConfigured.class)
+    @EnableJdbcHttpSession
+    public static class UaaJdbcSessionConfig extends UaaSessionConfig {
+        @Autowired
+        public void customizeIdleTimeout(
+                final JdbcHttpSessionConfiguration jdbcHttpSessionConfiguration,
+                final @Value("${servlet.idle-timeout:1800}") int idleTimeout) {
+            jdbcHttpSessionConfiguration.setMaxInactiveIntervalInSeconds(idleTimeout);
+        }
+    }
+
+    @Configuration
+    @Conditional(MemoryConfigured.class)
+    @EnableSpringHttpSession
+    public static class UaaMemorySessionConfig extends UaaSessionConfig {
+        @Bean
+        public MapSessionRepository sessionRepository(final @Value("${servlet.idle-timeout:1800}") int idleTimeout) {
+            MapSessionRepository sessionRepository = new MapSessionRepository(new ConcurrentHashMap<>());
+            sessionRepository.setDefaultMaxInactiveInterval(idleTimeout);
+            return sessionRepository;
+        }
+    }
+
+    public static class MemoryConfigured implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            String sessionStore = context.getEnvironment().getProperty("servlet.session-store");
+            return !DATABASE_SESSION_STORE_TYPE.equals(sessionStore);
+        }
+    }
+
+    public static class DatabaseConfigured implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            String sessionStore = context.getEnvironment().getProperty("servlet.session-store");
+            return DATABASE_SESSION_STORE_TYPE.equals(sessionStore);
+        }
+    }
+
     @Bean
     @RequestScope
     public CookieSerializer uaaCookieSerializer(
