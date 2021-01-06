26b4487cac8dd7d17cbe0b050b26d6306465c22f
Epshteyn <irina.epshteyn1@ge.com>
2021-01-06 15:07:28 -0800

    Session cookie SameSite policy must be None to support SAML/OIDC.

--- server/src/main/java/org/cloudfoundry/identity/uaa/web/beans/UaaSessionConfig.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/beans/UaaSessionConfig.java
@@ -4,15 +4,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.web.context.annotation.RequestScope;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class UaaSessionConfig {
-
     @Bean
+    @RequestScope
     public CookieSerializer uaaCookieSerializer(
-            final @Value("${servlet.session-cookie.max-age:-1}") int cookieMaxAge
+            final @Value("${servlet.session-cookie.max-age:-1}") int cookieMaxAge,
+            final HttpServletRequest request
     ) {
         DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
-        cookieSerializer.setSameSite(null);
+
+        if (request.isSecure()) {
+            cookieSerializer.setSameSite("None");
+        } else {
+            cookieSerializer.setSameSite(null);
+        }
+
         cookieSerializer.setCookieMaxAge(cookieMaxAge);
         cookieSerializer.setCookieName("JSESSIONID");
 
